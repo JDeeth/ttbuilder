c978150cfbb67b770f84b492e9a4b48c5b61d80a
Jack Deeth <developer@jackdeeth.org.uk>
2024-08-11 02:08:58 +0100

    Add most of train category implementation

--- src/train_types.py
+++ src/train_types.py
@@ -1,67 +1,16 @@
+from enum import Enum, Flag, auto
 from dataclasses import dataclass, fields
+import secrets
 from lxml import etree
 
 from cajontime import CajonTime
 from helper import pascal_case
 
-"""
-# Train types
-
-- Description
-- Length (m)
-- Accel/Brake Rate
-  - Very low (slow freight)
-  - Low (standard freight)
-  - Medium (intercity)
-  - High (commuter)
-  - Very high (light loco / metro)
-- Max speed (mph)
-- Weight
-  - Normal
-  - Light
-  - Heavy
-- Defensive driving
-- Power types (multiselect)
-  - Overhead AC
-  - 3rd rail
-  - 4th rail
-  - Diesel
-  - Overhead DC
-  - Tramway
-  - Sim 1-4
-- Speed classes: (multiselect)
-  - EPS-E, EPS-D, HST, EMU, DMU, SP, CS (CL67), MGR, TGV (CL373), Loco-H, Metro, WES (CL442), Tripcock, Steam, Sim 1-4
-- Use freight linespeeds
-- Can use freight lines
-- Dwell times
-  - Red signal move off
-  - Crew change
-  - Station forward
-  - Station reverse
-  - Terminate forward
-  - Terminate reverse
-  - Join
-  - Divide
-
-from wiki:
-<TrainCategory> 
-attribute ID 	an empty or missing value is replaced by a random 8 digit hexadecimal number
-<Description> 	default %%''%% 	
-<AccelBrakeIndex> 	default 0 	
-<IsFreight> 	default False 	use goods speeds
-<CanUseGoodsLines> 	default False 	
-<MaxSpeed> 	default 0 	
-<TrainLength> 	default 0 	
-<SpeedClass> 	default 0 	[SC]
-<PowerToWeightCategory> 	default 0 	{ Normal, Light, Heavy }
-<Electrification> 	default %%''%% 	[3]
-<CautionSpeedSet> 	default %%''%% 	contains the ID attribute of a <CautionSpeedSet>
-<DwellTimes> 	list 	contains <DwellTime>
-"""
-
 
 @dataclass
 class DwellTimes:
+    """Collection of dwell times for a SimSig train category"""
+
     red_signal_move_off: CajonTime = CajonTime(0)
     station_forward: CajonTime = CajonTime(0)
     station_reverse: CajonTime = CajonTime(0)
@@ -71,11 +20,142 @@ class DwellTimes:
     divide: CajonTime = CajonTime(0)
     crew_change: CajonTime = CajonTime(0)
 
+    def __post_init__(self):
+        """Assume plain-int values are seconds and convert"""
+        for field in fields(self):
+            value = getattr(self, field.name)
+            if isinstance(value, int):
+                setattr(self, field.name, CajonTime(value))
+
     def xml(self):
+        """Returns data in XML format"""
         result = etree.Element("DwellTimes")
         for field in fields(self):
             time = getattr(self, field.name)
             text = str(time.seconds)
             etree.SubElement(result, pascal_case(field.name)).text = text
+
+        return result
+
+
+class AccelBrake(Enum):
+    """SimSig train acceleration/braking categories"""
+
+    VERY_LOW = 0
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+    VERY_HIGH = 4
+
+
+class PowerType(Flag):
+    """SimSig traction power categories
+
+    Combine with |"""
+
+    NONE = 0
+    AC_OVERHEAD = auto()
+    DC_3RAIL = auto()
+    DC_4RAIL = auto()
+    DIESEL = auto()
+    DC_OVERHEAD = auto()
+    TRAMWAY = auto()
+    SIM_1 = auto()
+    SIM_2 = auto()
+    SIM_3 = auto()
+    SIM_4 = auto()
+
+    def str(self):
+        """Format as in timetable XML"""
+        if self not in (PowerType.NONE, PowerType.DIESEL):
+            raise NotImplementedError()
+        result = ""
+        if self & PowerType.DIESEL:
+            result += "D"
+        return result
+
+
+class SpeedClass(Flag):
+    """SimSig speed restriction/easement categories
+
+    Combine with |"""
+
+    EPS_E = 2**0
+    EPS_D = 2**1
+    HST = 2**2
+    EMU = 2**3
+    DMU = 2**4
+    SP = 2**5
+    CS_67 = 2**6
+    MGR = 2**7
+    TGV_373 = 2**8
+    LOCO_H = 2**9
+    METRO = 2**10
+    WES_442 = 2**11
+    TRIPCOCK = 2**12
+    STEAM = 2**13
+    SIM_1 = 2**14
+    SIM_2 = 2**15
+    SIM_3 = 2**16
+    SIM_4 = 2**17
+
+
+Mph = int
+Meters = int
+
+
+class Weight(Enum):
+    """SimSig train weight"""
+
+    LIGHT = None
+    NORMAL = 0
+    HEAVY = None
+
+
+@dataclass
+class TrainType:
+    """SimSig train category as represented in timetable files"""
+
+    description: str = ""
+    length: Meters = 0
+    max_speed: Mph = 0
+
+    id: str = None
+
+    accel: AccelBrake = AccelBrake.MEDIUM
+    weight: Weight = Weight.NORMAL
+    defensive_driving: None = None
+
+    power_type: PowerType = PowerType(0)
+    speed_classes: SpeedClass = SpeedClass(0)
+
+    use_freight_linespeeds: bool = False
+    can_use_freight_lines: bool = False
+
+    dwell_times: DwellTimes = DwellTimes()
+
+    def __post_init__(self):
+        if self.id is None:
+            self.id = secrets.token_hex(4)
+
+    def xml(self):
+        """XML as used in SimSig timetables"""
+        result = etree.Element("TrainCategory", ID=self.id)
+
+        def subelem(tag, value):
+            if isinstance(value, bool):
+                value = 1 if value else 0
+            etree.SubElement(result, tag).text = str(value)
+
+        subelem("Description", self.description)
+        subelem("AccelBrakeIndex", self.accel.value)
+        subelem("IsFreight", self.use_freight_linespeeds)
+        subelem("CanUseGoodsLines", self.can_use_freight_lines)
+        subelem("MaxSpeed", self.max_speed)
+        subelem("TrainLength", self.length)
+        subelem("SpeedClass", self.speed_classes.value)
+        subelem("PowerToWeightCategory", self.weight.value)
+        result.append(self.dwell_times.xml())
+        subelem("Electrification", self.power_type.str())
 
         return result
--- tests/test_train_types.py
+++ tests/test_train_types.py
@@ -1,7 +1,7 @@
-from cajontime import CajonTime
 import pytest
 
-from train_types import DwellTimes
+from cajontime import CajonTime
+from train_types import AccelBrake, DwellTimes, PowerType, SpeedClass, TrainType
 
 empty_dt = """
 <DwellTimes>
@@ -37,6 +37,11 @@ def test_default_dwell_times(xml_test_tools):
     assert xt.agnostic_diff(expected, dt.xml()) == []
 
 
+def test_dwell_time_can_populate_from_int():
+    dt = DwellTimes(terminate_forward=150)
+    assert dt.terminate_forward == CajonTime.from_hms(minutes=2, seconds=30)
+
+
 def test_populated_dwell_times(xml_test_tools):
     xt = xml_test_tools
     dt = DwellTimes(
@@ -51,3 +56,52 @@ def test_populated_dwell_times(xml_test_tools):
     dt.crew_change = CajonTime.from_str("0:05")
     expected = xt.fromstr(populated_dt)
     assert xt.agnostic_diff(expected, dt.xml()) == []
+
+
+dmu_traintype = """
+<TrainCategory ID="23F09234">
+    <Description>3-car DMU</Description>
+    <AccelBrakeIndex>3</AccelBrakeIndex>
+    <IsFreight>0</IsFreight>
+    <CanUseGoodsLines>0</CanUseGoodsLines>
+    <MaxSpeed>70</MaxSpeed>
+    <TrainLength>60</TrainLength>
+    <SpeedClass>16</SpeedClass>
+    <PowerToWeightCategory>0</PowerToWeightCategory>
+    <DwellTimes>
+        <RedSignalMoveOff>10</RedSignalMoveOff>
+        <StationForward>45</StationForward>
+        <StationReverse>180</StationReverse>
+        <TerminateForward>60</TerminateForward>
+        <TerminateReverse>240</TerminateReverse>
+        <Join>300</Join>
+        <Divide>120</Divide>
+        <CrewChange>300</CrewChange>
+    </DwellTimes>
+    <Electrification>D</Electrification>
+</TrainCategory>
+"""
+
+
+def test_train_types_get_unique_default_ids():
+    tt1 = TrainType()
+    tt2 = TrainType()
+    assert tt1.id != tt2.id
+
+
+# @pytest.mark.xfail
+def test_make_dmu_train_type(xml_test_tools):
+    xt = xml_test_tools
+    dmu_dwell_times = DwellTimes(10, 45, 180, 60, 240, 300, 120, 300)
+    dmu = TrainType(
+        id="23F09234",
+        description="3-car DMU",
+        length=60,
+        accel=AccelBrake.HIGH,
+        max_speed=70,
+        speed_classes=SpeedClass.DMU,
+        dwell_times=dmu_dwell_times,
+        power_type=PowerType.DIESEL,
+    )
+    expected = xt.fromstr(dmu_traintype)
+    assert xt.agnostic_diff(expected, dmu.xml()) == []
