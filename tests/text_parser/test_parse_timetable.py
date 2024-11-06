39a706a6460505281d9c521889ec66b10f35dd34
Jack Deeth <developer@jackdeeth.org.uk>
2024-11-06 01:51:12 +0000

    Fix timetable h/H/½ parsing

--- tests/text_parser/test_parse_timetable.py
+++ tests/text_parser/test_parse_timetable.py
@@ -1,29 +1,18 @@
-import pytest
 from ttbuilder.common.ttime import TTime
 from ttbuilder.simsig.local_timetable import LocalTimetable
 
-TEST_1M45_WITH_H = """\
+TEST_1M45 = """\
 1M45
 22:30 Peterborough-Carlisle (Parcels)
 DRBY.4  00:40
-STSNJN  00/47h [1h]
-NSJDRBY 00/48h
-BURTNOT 00/53
-"""
-
-
-TEST_1M45_WITH_HALF = """\
-1M45
-22:30 Peterborough-Carlisle (Parcels)
-DRBY.4  00:40
-STSNJN  00/47½ [1½]
+STSNJN  00/47h [1H]
 NSJDRBY 00/48½
 BURTNOT 00/53
 """
 
 
 def test_timetable_from_str(ttparser):
-    tt: LocalTimetable = ttparser.parse_timetable(TEST_1M45_WITH_HALF)
+    tt: LocalTimetable = ttparser.parse_timetable(TEST_1M45)
     assert tt.train_id.id == "1M45"
     assert tt.origin == "Peterborough"
     assert tt.destination == "Carlisle"
@@ -36,8 +25,3 @@ def test_timetable_from_str(ttparser):
     assert drby.location.tiploc == "DRBY"
     assert drby.location.platform == "4"
     assert drby.depart == TTime.stopping(0, 40)
-
-
-@pytest.mark.xfail(reason="Unknown why this causes a parsing error?")
-def test_timetable_from_str_with_h(ttparser):
-    ttparser.parse_timetable(TEST_1M45_WITH_H)
